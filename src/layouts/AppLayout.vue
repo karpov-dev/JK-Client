ff5e45191f66c00b85ed4486a46cd12f0cfa3c2d
Uladzimir Karpau <uladzimir.karpau@gmail.com>
2022-01-25 12:34:37 +0300

    v. 0.0.1.2.1 Added

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -1,17 +1,27 @@
 <template>
-  <div id="app">
-
+  <div id="app" :data-theme="themeName">
     <div class="wrapper">
       <transition name="fade">
         <router-view></router-view>
       </transition>
     </div>
-
   </div>
 </template>
 
 <script>
-  export default {
-    name: "AppLayout"
-  }
+import store from "../store";
+
+export default {
+  name: "AppLayout",
+  data() {
+    return {
+      themeName: store.getters["storage/getTheme"],
+    };
+  },
+};
 </script>
+
+<style scoped lang="scss">
+@import url("../scss/themes/light.scss");
+@import url("../scss/themes/dark.scss");
+</style>
