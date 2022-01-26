f6d6919370105a8d31aac88a9bb651bf48f95414
Uladzimir Karpau <uladzimir.karpau@gmail.com>
2022-01-26 16:44:10 +0300

    v. 0.0.1.6 Added base and simple cards

---
+++ src/components/base/BaseCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="base-bard">
+    <div class="base-bard__header">
+      <slot name="header"></slot>
+    </div>
+
+    <div class="base-bard__content">
+      <slot></slot>
+    </div>
+
+    <div class="base-bard__footer">
+      <slot name="footer"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "BaseCard"
+  }
+</script>
+
+<style scoped lang="scss">
+  .base-bard {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    border: 0;
+    padding: var(--padding__medium);
+    border-radius: var(--border-radius__medium);
+    background-color: var(--color-background__card);
+    box-shadow: var(--box-shadow__medium);
+  }
+</style>
---
+++ src/components/card/CardSimple.vue
@@ -0,0 +1,66 @@
+<template>
+  <base-card>
+    <template v-slot:header>
+      <div class="card-simple__header">
+        <div class="card-simple__header-title">
+          <slot name="card-title">
+            <div v-html="title"></div>
+          </slot>
+        </div>
+
+        <div class="card-simple__header-description">
+          <slot name="card-description">
+            <div v-html="description"></div>
+          </slot>
+        </div>
+      </div>
+    </template>
+
+    <slot></slot>
+
+    <template v-slot:footer>
+      <slot name="footer"></slot>
+    </template>
+  </base-card>
+</template>
+
+<script>
+  import BaseCard from "../base/BaseCard";
+
+  export default {
+    name: "CardSimple",
+    components: {BaseCard},
+    props: {
+      title: {
+        type: String,
+        default: null
+      },
+
+      description: {
+        type: String,
+        default: null
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  .card-simple__header {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    padding-bottom: var(--margin__large-xx);
+  }
+
+  .card-simple__header-title {
+    text-align: center;
+    font-family: var(--font-famaly__viga);
+    font-size: var(--font-size__large-x);
+    padding-bottom: var(--margin__small-xxx);
+  }
+
+  .card-simple__header-description {
+    text-align: center;
+    font-size: var(--font-size__large);
+  }
+</style>
