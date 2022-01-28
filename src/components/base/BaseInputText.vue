08d9ddf90020ef50bc0ebcd29146cb774d634c56
Uladzimir Karpau <uladzimir.karpau@gmail.com>
2022-01-28 17:50:40 +0300

    v. 0.0.4.2 Added base input

---
+++ src/components/base/BaseInputText.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="input__container">
+    <slot name="before"></slot>
+    <input>
+    <slot name="after"></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "BaseInputText"
+  }
+</script>
+
+<style scoped>
+  input {
+    color: var(--color-font);
+    background: transparent;
+    border-width: 0;
+    width: 100%;
+    box-sizing: border-box;
+    padding: var(--padding__medium) var(--padding__large);
+  }
+
+  .input__container {
+    border-radius: var(--border-radius__small);
+    background-color: var(--color-input__background);
+  }
+</style>
