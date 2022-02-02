42ccfd355f5dfe5b8fde2d852bfb44a995de832b
Uladzimir Karpau <uladzimir.karpau@gmail.com>
2022-02-02 16:29:43 +0300

    v. 0.0.6 - added form to request restore password - added form to restore password

---
+++ src/components/form/formRestorePassword.vue
@@ -0,0 +1,28 @@
+<template>
+  <ui-card-simple title="Restore password" description="Fill new password">
+    <div class="card__content">
+      <ui-input name="password" title="Password"></ui-input>
+      <ui-input name="repeatPassword" title="Repeat password"></ui-input>
+
+      <ui-button variant="success">Restore</ui-button>
+    </div>
+  </ui-card-simple>
+</template>
+
+<script>
+  import UiCardSimple from "../ui/card/UiCardSimple";
+  import UiInput from "../ui/input/UiInput";
+  import UiButton from "../ui/button/UiButton";
+  export default {
+    name: "formRestorePassword",
+    components: {UiButton, UiInput, UiCardSimple}
+  }
+</script>
+
+<style scoped>
+  .card__content {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+</style>
---
+++ src/components/form/formRestorePasswordRequest.vue
@@ -0,0 +1,28 @@
+<template>
+  <ui-card-simple title="Restore Password Request" description="Fill your email to restore password">
+    <div class="card__content">
+      <ui-input name="email" title="Email"></ui-input>
+
+      <ui-button variant="common">Send Email</ui-button>
+    </div>
+  </ui-card-simple>
+</template>
+
+<script>
+  import UiCardSimple from "../ui/card/UiCardSimple";
+  import UiInput from "../ui/input/UiInput";
+  import UiButton from "../ui/button/UiButton";
+
+  export default {
+    name: "formRestorePasswordRequest",
+    components: {UiButton, UiInput, UiCardSimple}
+  }
+</script>
+
+<style scoped>
+  .card__content {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+</style>
