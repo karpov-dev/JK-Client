a3d45cc19747c9bfd991ba53a58a1b22d8236b6a
Uladzimir Karpau <uladzimir.karpau@gmail.com>
2022-03-19 18:43:04 +0300

    v. 0.1.4 - button group fix

--- src/components/ui/button/UiButtonGroup.vue
+++ src/components/ui/button/UiButtonGroup.vue
@@ -18,30 +18,30 @@
 
   .button-group::v-deep {
 
-    .button:not(:first-of-type):not(:last-of-type) {
+    .button-group__allowed:not(:first-of-type):not(:last-of-type) * {
       border-radius: 0;
       border-left-width: 0;
       border-right-width: 0;
     }
 
-    .button:not(:last-of-type):first-of-type {
+    .button-group__allowed:not(:last-of-type):first-of-type * {
       border-right-width: 0;
       border-top-right-radius: 0;
       border-bottom-right-radius: 0;
     }
 
-    .button:not(:first-of-type):last-of-type {
+    .button-group__allowed:not(:first-of-type):last-of-type * {
       border-left-width: 0;
       border-top-left-radius: 0;
       border-bottom-left-radius: 0;
     }
 
-    .button:first-of-type {
+    .button-group__allowed:first-of-type * {
       border-top-left-radius: 6px;
       border-bottom-left-radius: 6px;
     }
 
-    .button:last-of-type {
+    .button-group__allowed:last-of-type * {
       border-top-right-radius: 6px;
       border-bottom-right-radius: 6px;
     }
